f68ae5452701089e2c0e7150636484c2214b8448
Juan Martinez <pablomar01@outlook.com>
2024-07-30 09:53:33 -0500

    Create layout

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,21 @@
 </script>
 
 <template>
-  <RouterView />
+  <v-card elevation="3" class="mx-auto" max-width="1200">
+    <v-layout>
+      <v-app-bar color="blue-darken-1">
+        <template v-slot:prepend>
+          <v-btn :to="{ name: 'home' }">
+            FireState - Home
+          </v-btn>
+        </template>
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <RouterView />
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
