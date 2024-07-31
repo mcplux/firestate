aab6d55fb6f720449ec4bf6fb8638821cb83c1bc
Juan Martinez <pablomar01@outlook.com>
2024-07-31 09:35:23 -0500

    Validate form with VeeValidate

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
+import { useField, useForm } from 'vee-validate'
+import { loginSchema } from '@/validation/loginSchema'
 
+const { handleSubmit } = useForm({ validationSchema: loginSchema })
+
+const email = useField('email')
+const password = useField('password')
+
+console.log(email)
+
+const submit = handleSubmit(() => console.log('Sending...'))
 </script>
 
 <template>
@@ -13,15 +23,21 @@
         type="email"
         label="Email"
         bg-color="blue-grey-lighten-5"
+        class="mb-3"
+        v-model="email.value.value"
+        :error-messages="email.errorMessage.value"
       />
 
       <v-text-field 
         type="password"
         label="Password"
         bg-color="blue-grey-lighten-5"
+        class="mb-3"
+        v-model="password.value.value"
+        :error-messages="password.errorMessage.value"
       />
 
-      <v-btn block color="pink-accent-3">
+      <v-btn block color="pink-accent-3" @click="submit" class="mb-3">
         Login
       </v-btn>
     </v-form>
