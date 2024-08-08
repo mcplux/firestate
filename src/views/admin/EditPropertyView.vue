5df4bb06d5f9b6e199b3334e2ac0fdcb0d85816a
Juan Martinez <pablomar01@outlook.com>
2024-08-08 11:13:02 -0500

    Get property to edit

--- src/views/admin/EditPropertyView.vue
+++ src/views/admin/EditPropertyView.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { doc } from 'firebase/firestore'
+import { useDocument, useFirestore, type _RefFirestore } from 'vuefire'
+import type { Property } from '@/interfaces/property.interface'
 
+const route = useRoute()
+const db = useFirestore()
+
+const id = route.params.id as string
+
+// Get the property
+const docRef = doc(db, 'properties', id)
+const property = useDocument(docRef) as _RefFirestore<Property | undefined>
+
+console.log(property)
 </script>
 
 <template>
