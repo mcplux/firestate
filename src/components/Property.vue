1edbcb0a8dc525278bc8a57ccb2fb521466a3d6a
Juan Martinez <pablomar01@outlook.com>
2024-08-12 08:16:07 -0500

    List properties in the public area

---
+++ src/components/Property.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Property } from '@/interfaces/property.interface'
+import { formatCurrency } from '@/helpers'
+
+interface Props {
+  property: Property
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <v-col cols="12" md="4">
+    <v-card>
+      <v-img :src="property.image" height="250" cover />
+      <v-card-title>{{ property.title }}</v-card-title>
+      <v-card-text class="text-truncate">{{ property.description }}</v-card-text>
+      <v-card-text class="text-truncate text-h5 font-weight-bold">{{ formatCurrency(property.price) }}</v-card-text>
+      <template v-slot:actions>
+        <v-btn block color="info" variant="tonal" prepend-icon="mdi-information-outline">More info</v-btn>
+      </template>
+    </v-card>
+  </v-col>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import useProperties from '@/composables/useProperties'
+import Property from '@/components/Property.vue'
 
+const { propertiesCollection } = useProperties()
 </script>
 
 <template>
-  <h1 class="text-center text-h3 font-weight-bold my-5">
-    FireState
-  </h1>
+  <v-card flat>
+    <v-card-title class="text-h3 text-center" tag="h1">
+      Properties
+    </v-card-title>
+
+    <v-row>
+      <Property 
+        v-for="property in propertiesCollection"
+        :key="property.id"
+        :property="property"
+      />
+    </v-row>
+  </v-card>
 </template>
