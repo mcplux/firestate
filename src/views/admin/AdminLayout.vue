4527132b00f500d7ba86cf8f6aac27803806c9a1
Juan Martinez <pablomar01@outlook.com>
2024-08-13 18:37:00 -0500

    fix: Redirect when visits AdminLayout route

--- src/views/admin/AdminLayout.vue
+++ src/views/admin/AdminLayout.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
+
+onMounted(() => {
+  router.push({ name: 'admin-properties' })
+})
 </script>
 
 <template>
